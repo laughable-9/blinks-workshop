05df300c94a08ff18da3ad955b8f4bb774dd1524
Kyle Pagunsan <clarencekyl3@gmail.com>
2024-07-20 21:59:33 +0800

    Update website

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,49 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className="flex flex-col pl-24 min-h-screen items-center justify-center text-white bg-black-100">
+      <div className="flex w-full justify-left items-center mb-8">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="https://i.imgur.com/wKY2gEc.jpeg"
-          alt="Superteam Philippines BUIDLSTATION"
-          width={500}
-          height={500}
-          priority
+          className="rounded-lg"
+          src="https://i.imgur.com/gSFtFCJ.png"
+          alt="Trophy"
+          width={100}
+          height={50}
         />
       </div>
+      <div className="flex flex-col items-center">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold">Kitten Coup #2139</h1>
+        </div>
+        <div className="flex">
+          <div className="relative mb-8 mr-8">
+            <Image
+              className="rounded-lg"
+              src="https://i.imgur.com/InBPg5a.png"
+              alt="Kitten Coup #2139"
+              width={300}
+              height={300}
+            />
+          </div>
+          <div className=" p-8 rounded-lg">
+            <h2 className="text-2xl mb-4">Participants (11)</h2>
+            <ul className="list-none space-y-2">
+              <li className="bg-white-100 pl-4 pr-24 rounded-3xl">GpPSNu4FrEtdtW7rDs8s3twyAinTMEbz3SqaNrh9Bp6</li>
+              <li className="bg-white-100 pl-4 pr-24 rounded-3xl">0xdb676b49cb0Df7A375393F90035B2a6afd4E8054</li>
+              <li className="bg-white-100 pl-4 pr-24 rounded-3xl">0xdb676b49cb0Df7A375393F90035B2a6afd4E8054</li>
+              <li className="bg-white-100 pl-4 pr-24 rounded-3xl">bc1qzp407zevtm2u9dx2dyp5er4ytutz2raf089xtv</li>
+              <li className="bg-white-100 pl-4 pr-24 rounded-3xl">GpPSNu4FrEtdtW7rDs8s3twyAinTMEbz3SqaNrh9Bp6</li>
+              <li className="bg-white-100 pl-4 pr-24 rounded-3xl">bc1qzp407zevtm2u9dx2dyp5er4ytutz2raf089xtv</li>
+            </ul>
+          </div>
+
+        </div>
+        <div className="flex mt-8 space-x-4">
+          <button className="bg-black-100 pl-4 pr-4 pt-2 pb-2 rounded-3xl hover:bg-white-200 hover:text-black-100 border">Reset Giveaway</button>
+          <button className="bg-black-100 pl-4 pr-4 pt-2 pb-2 rounded-3xl hover:bg-white-200 hover:text-black-100 border">End Giveaway</button>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#352f2f",
+        black: {
+          100: "#352f2f",
+          200: "#888883",
+        },
+        white: {
+          100: "rgba(255, 255, 255, 0.1)",
+          200: "rgba(255, 255, 255, 1)",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
